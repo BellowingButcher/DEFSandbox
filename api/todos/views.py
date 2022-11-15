6514c937f8051441d81ccd5c54b3c8f796567b89
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-11-15 18:02:20 +0000

    building up the views for my api call

--- api/todos/views.py
+++ api/todos/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http.response import Http404
+from rest_framework.views import APIView
+from .models import Todo
+from .serializers import TodoSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+class TodoAPIView(APIView):
+    def get_object(self, pk):
+        try:
+            return Todo.objects.get(pk=pk)
+        except Todo.DoesNotExist:
+            raise Http404
+
+    def get_all_objects(self, request, pk=None, format=None):
+        if pk:
+            data = self.get_object(pk)
+            serializer = TodoSerializer(data)
+
+        else:
+            data = Todo.objects.all()
